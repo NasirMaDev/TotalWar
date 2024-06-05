a5fb91e39c30492df7e9118240482d975162962f
Nasir <Nasirmadev@gmail.com>
2024-06-05 20:25:56 +0500

    fix : button issue on release mode

--- PanelDashBoard/Extensions/Sources/Extensions/UIView+Extensions.swift
+++ PanelDashBoard/Extensions/Sources/Extensions/UIView+Extensions.swift
@@ -97,42 +97,6 @@ extension UIView {
 
 
 
-extension UIButton {
-    
-    func setGradientBackground(colors: [UIColor], locations: [NSNumber]? = nil, startPoint: CGPoint = CGPoint(x: 0.0, y: 0.5), endPoint: CGPoint = CGPoint(x: 1.0, y: 0.5)) {
-        // Remove any existing gradient layers to avoid layering multiple gradients
-        layer.sublayers?.forEach {
-            if $0 is CAGradientLayer {
-                $0.removeFromSuperlayer()
-            }
-        }
-        
-        // Create a gradient layer
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.colors = colors.map { $0.cgColor }
-        gradientLayer.locations = locations
-        gradientLayer.startPoint = startPoint
-        gradientLayer.endPoint = endPoint
-        gradientLayer.frame = self.bounds
-        gradientLayer.cornerRadius = self.layer.cornerRadius
-        
-        // Add the gradient layer to the button's layer
-        self.layer.insertSublayer(gradientLayer, at: 0)
-        
-        // Optional: If you need to update the gradient when the button's size changes, you can add constraints or override layoutSubviews to update the gradient layer's frame.
-    }
-    
-    override open func layoutSubviews() {
-        super.layoutSubviews()
-        
-        // Update gradient layer frame when button's layout changes
-        if let gradientLayer = self.layer.sublayers?.first(where: { $0 is CAGradientLayer }) as? CAGradientLayer {
-            gradientLayer.frame = self.bounds
-        }
-    }
-}
-
-
 
 extension UIImageView {
     func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit, completion: @escaping (Result<UIImage, Error>) -> Void) {
